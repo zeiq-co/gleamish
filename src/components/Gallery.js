3ce2f31eca29ff407ba16a80479d6e96be2e0215
iamkunall <103kunalsharma@gmail.com>
2020-12-05 13:48:58 +0530

    gallery tab fixed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,60 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import GalleryCard from './GalleryCard';
+import TabButton from './TabButtons';
 
-const GalleryWrapper = styled.div`
-  .button {
-    box-shadow: none;
-    border-radius: 0;
-    background-color: #f1f5f7;
-    border-color: #f1f5f7;
-  }
-`;
+const GalleryWrapper = styled.div``;
 
 const GalleryItem = [
   {
     id: 1,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery1.jpg',
+    category: 'Exterior',
   },
   {
     id: 2,
     image:
       ' https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery2.jpg',
+    category: 'Exterior',
   },
   {
     id: 3,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery3.jpg',
+    category: 'Exterior',
   },
   {
     id: 4,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery4.jpg',
+    category: 'Interior',
   },
   {
     id: 5,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery5.jpg',
+    category: 'Interior',
   },
   {
     id: 6,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery6.jpg',
+    category: 'Interior',
   },
   {
     id: 7,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery7.jpg',
+    category: 'Stairwell',
   },
   {
     id: 8,
     image:
       'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery8.jpg',
+    category: 'Stairwell',
+  },
+  {
+    id: 9,
+    image:
+      'https://3put9a43ycne3koyi63vujgg-wpengine.netdna-ssl.com/wp-content/uploads/2016/03/gallery8.jpg',
+    category: 'Stairwell',
   },
 ];
 
 const Gallery = () => {
+  const [category, setCategory] = useState('All');
   return (
     <GalleryWrapper>
       <section className="section">
@@ -65,33 +74,36 @@ const Gallery = () => {
           <div className="columns  is-centered">
             <div className="column is-6">
               <div className="columns is-centered">
-                <div className="column">
-                  <button class="button is-medium is-fullwidth">
-                    Filter - All
-                  </button>
-                </div>
-                <div className="column">
-                  <button class="button is-medium is-fullwidth">
-                    Exterior
-                  </button>
-                </div>
-                <div className="column">
-                  <button class="button is-medium is-fullwidth">
-                    Interior
-                  </button>
-                </div>
-                <div className="column">
-                  <button class="button is-medium is-fullwidth">
-                    Stairwell
-                  </button>
-                </div>
+                <TabButton
+                  title="All"
+                  onClick={() => setCategory('All')}
+                  active={category}
+                />
+                <TabButton
+                  title="Exterior"
+                  onClick={() => setCategory('Exterior')}
+                  active={category}
+                />
+                <TabButton
+                  title="Interior"
+                  onClick={() => setCategory('Interior')}
+                  active={category}
+                />
+                <TabButton
+                  title="Stairwell"
+                  onClick={() => setCategory('Stairwell')}
+                  active={category}
+                />
               </div>
             </div>
           </div>
           <div className="columns  is-multiline is-mobile">
-            {GalleryItem.map((item) => (
-              <GalleryCard key={item.id} image={item.image} />
-            ))}
+            {GalleryItem.map((item) => {
+              if (category !== 'All' && category !== item.category) {
+                return null;
+              }
+              return <GalleryCard key={item.id} image={item.image} />;
+            })}
           </div>
         </div>
       </section>
---
+++ src/components/TabButtons.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  box-shadow: none;
+  border-radius: 0;
+  background-color: ${(props) =>
+    props.active ? '#ebc330' : '#f1f5f7'} !important;
+  border-color: #f1f5f7;
+  transition: all 0.5s ease-out;
+  :focus {
+    box-shadow: none !important;
+    border-color: transparent !important;
+  }
+  :hover {
+    border-color: #ebc330 !important;
+    border-width: 0.5px;
+  }
+`;
+
+const TabButton = ({ title, onClick, active }) => {
+  console.log(title == active);
+  return (
+    <div className="column">
+      <StyledButton
+        type="button"
+        className="button is-medium is-fullwidth"
+        onClick={onClick}
+        active={title == active}
+      >
+        {title}
+      </StyledButton>
+    </div>
+  );
+};
+export default TabButton;
