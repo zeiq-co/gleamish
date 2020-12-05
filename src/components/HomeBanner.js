e22ed0963eb8967b7e0a1290f2172f86a0c3131e
karanbir8872 <wwwkaranbir786@gmail.com>
2020-12-05 16:26:17 +0530

    minor changes

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -6,36 +6,39 @@ const Section = styled.div`
   background-image: url(${(props) => props.bgImage});
   background-repeat: no-repeat;
   background-size: cover;
+  display: flex;
+  align-items: center;
+  height: 100vh;
   .heading {
     font-size: 70px !important;
+    line-height: 5rem;
   }
   .subheading {
     font-size: 18px !important;
+    line-height: 27px;
   }
-  button{
+  button {
     padding: 1.2rem 4rem;
     border-radius: 0;
-}
+  }
+  .columns {
+    flex: 1;
   }
 `;
 
 const HomeBanner = ({ bgImage }) => {
   return (
-    <Section
-      className="hero is-large is-large-widescreen is-large-fullhd"
-      bgImage={bgImage}
-    >
-      <div className="hero-body">
-        <div className="columns is-centered">
-          <div className="column is-5 has-text-centered">
-            <h1 class="heading has-text-weight-normal has-text-white mb-5">
-              Proven Reliability
-            </h1>
-            <h1 class="subheading is-size-5 has-text-weight-normal has-text-white mb-5">
-              We Value Work Ethic & Industry Standard
-            </h1>
-            <button class="button is-medium">Get Started</button>
-          </div>
+    <Section className="" bgImage={bgImage}>
+      <div className="columns is-centered is-vcentered">
+        <div className="column is-5 has-text-centered">
+          <h1 class="heading has-text-weight-normal has-text-white mb-4">
+            Lorem Ipsum
+          </h1>
+          <h1 class="subheading is-size-5 has-text-weight-normal has-text-white mb-5">
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit...
+          </h1>
+          <button class="button is-medium">Get Started</button>
         </div>
       </div>
     </Section>
