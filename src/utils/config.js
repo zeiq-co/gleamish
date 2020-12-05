a92ae052342de05ac5f4bb5449c7947e8e55a451
dharmveer97 <dvbangar8@gmail.com>
2020-12-05 22:51:37 +0530

    minor fixes..

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,9 +7,9 @@ module.exports = {
   siteUrl: 'https://www.google.com/',
   logo: '/images/logo.jpg',
 
-  telephone: '+1-123-123-123',
-  email: 'painting123@gmail.com',
-  address: 'California, USA',
+  telephone: '7 87 753 780',
+  email: 'Midaspainting@gmail.com',
+  address: '58, Thomson Street, USA',
   twitter: '@NAME-HERE',
   fbUserId: '@NAME-HERE',
   fbAppID: '@NAME-HERE',
