f24a3c2ac321c7eb6d35e95fc11335a98e05d3e6
dharmveer97 <dvbangar8@gmail.com>
2020-12-05 15:39:43 +0530

    minor bug fixes.

--- src/components/elements/ImageOverlay.js
+++ src/components/elements/ImageOverlay.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import BeforeAfterSlider from 'react-before-after-slider';
+// import Image from 'gatsby-image';
 import Heading from './Heading';
 
 const Section = styled.div``;
-const ImageOverlay = () => {
-  const before =
-    'http://prorange.ancorathemes.com/wp-content/uploads/2017/03/a2.jpg';
-  const after =
-    'http://prorange.ancorathemes.com/wp-content/uploads/2017/03/a1-1.jpg';
+const ImageOverlay = ({ Image }) => {
+  const before = '/images/before.jpg';
+  const after = '/images/after.jpg';
 
   return (
     <Section className="section">
@@ -20,12 +19,14 @@ const ImageOverlay = () => {
             </div>
           </div>
           <div className="column is-5">
-            <BeforeAfterSlider
-              before={before}
-              after={after}
-              width={640}
-              height={480}
-            />
+            {Image && (
+              <BeforeAfterSlider
+                before={before}
+                after={after}
+                width={640}
+                height={480}
+              />
+            )}
           </div>
         </div>
       </div>
