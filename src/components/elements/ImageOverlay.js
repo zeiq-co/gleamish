1c3b1937ff96aca7871d9c25b2c39eda44dd0984
dharmveer97 <dvbangar8@gmail.com>
2020-12-05 15:49:58 +0530

    minor bu fixes.

--- src/components/elements/ImageOverlay.js
+++ src/components/elements/ImageOverlay.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import BeforeAfterSlider from 'react-before-after-slider';
-// import Image from 'gatsby-image';
+import ReactCompareImage from 'react-compare-image';
 import Heading from './Heading';
 
 const Section = styled.div``;
-const ImageOverlay = ({ Image }) => {
-  const before = '/images/before.jpg';
-  const after = '/images/after.jpg';
 
+const ImageOverlay = () => {
   return (
     <Section className="section">
       <div className="container">
         <div className="columns">
-          <div className="column is-7">
+          <div className="column is-6">
             <div className="box">
               <Heading centered>relative</Heading>
             </div>
           </div>
-          <div className="column is-5">
-            {Image && (
-              <BeforeAfterSlider
-                before={before}
-                after={after}
-                width={640}
-                height={480}
-              />
-            )}
+          <div className="column is-6">
+            <ReactCompareImage
+              leftImage="/images/before.jpg"
+              rightImage="/images/after.jpg"
+            />
           </div>
         </div>
       </div>
