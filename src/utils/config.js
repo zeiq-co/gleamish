a4957585fde63b57f3e76bd5e5fba2a3bc508c92
dharmveer97 <dvbangar8@gmail.com>
2021-01-09 16:55:16 +0530

    fix.

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,7 @@ module.exports = {
   description:
     'Welcome to Midas Brush Decorating, a friendly and reliable service who serve customers throughout London producing the finest quality at competitive rates. We transform both residential and commercial properties inside and out with our unique Midas touch and our tremendous eye for detail.Our aim is to achieve one hundred percent satisfaction with our customers, making sure that we are clean and tidy during work and when we complete our projects.',
   siteUrl: 'https://www.paint.com/',
-  logo: '../../static/images/logo.png',
+  logo: '/images/logo.png',
 
   telephone: '07572575313',
   email: 'eldan@midasbrushdecorating.co.uk',
