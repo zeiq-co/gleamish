b00aafde43e4169258c4468d24f19d3cdb5cda40
dharmveer97 <dvbangar8@gmail.com>
2021-01-10 00:12:36 +0530

    fix

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ModalImage from 'react-modal-image';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  .__react_modal_image__medium_img {
+    height: 500px !important;
+    max-width: 100% !important;
+  }
+`;
 
 const GalleryCard = ({ src, alt }) => {
   return (
-    <div className="column is-4">
+    <Container className="column is-4">
       <ModalImage
         large={src}
         alt={alt}
         small={src}
         className="modal_small_image"
       />
-    </div>
+    </Container>
   );
 };
 export default GalleryCard;
