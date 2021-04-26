2f9b21dcf6bc7dba79efd429c96ed8bbf5539dac
dharmveer97 <dvbangar8@gmail.com>
2021-04-26 12:43:47 +0530

    fix

--- dashboardConfig.js
+++ dashboardConfig.js
@@ -12,19 +12,5 @@ export default {
         width: 'medium',
       },
     },
-    {
-      name: 'netlify',
-      options: {
-        title: 'My Netlify deploys',
-        sites: [
-          {
-            title: 'Paint Sanity Studio',
-            apiId: '',
-            buildHookId: 'https://api.netlify.com/build_hooks/5ffd3f7e495938b3f6f21e81',
-            name: 'midasbrush-decorating.',
-          },
-        ],
-      },
-    },
   ],
 };
